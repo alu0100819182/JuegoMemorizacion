02c817717ed3397c06b37268eafa748de59a8d7b
Omar Adolfo Alvarez Hernandez <alu0100819182@ull.edu.es>
2016-10-18 19:34:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,10 +21,16 @@ function mensaje() {
   swal({
     title: 'Título',
     text: 'Mensaje de texto',
-    html: '<p>Mensaje de texto con <strong>formato</strong>.</p>',
     type: 'success',
-    timer: 3000,
-  });
+    showCloseButton: true,
+    confirmButtonText: '<i class="fa fa-thumbs-up"></i> ¡Bien!',
+    confirmButtonColor: '#1db75a',
+    showCancelButton: true,
+    cancelButtonText: '<i class="fa fa-thumbs-down"></i> :(',
+    }).then(
+      function() { swal('¡Aceptado!') },
+      function() { swal('¡Cancelado!') }
+  );
 }
 
 

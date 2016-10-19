b83788c93cef05dec09286ee781f6b806845cc3d
Omar Adolfo Alvarez Hernandez <alu0100819182@ull.edu.es>
2016-10-19 18:48:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -91,6 +91,19 @@ $('.editor').trumbowyg({
 
 $('#trumbowyg').trumbowyg('html'); // Obtienes el HTML generado
 
+$('.editor').trumbowyg({
+  lang: 'es', // Idioma (es.min.js incluido)
+  btns: [
+          ['bold', 'italic', 'underline', 'strikethrough'],
+          ['superscript', 'subscript'],
+          ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+          ['unorderedList', 'orderedList'],
+          ['link', 'insertImage'],
+          ['horizontalRule', 'removeformat'],
+          ['viewHTML'],
+          ['fullscreen']
+        ],
+});
 
 
 

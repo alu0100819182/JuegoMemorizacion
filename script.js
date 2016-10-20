177f55600fc8e8772600de9a19ba248a61f2ce20
Omar Adolfo Alvarez Hernandez <alu0100819182@ull.edu.es>
2016-10-20 16:16:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -128,17 +128,18 @@ $(document).ready(function() {
 });
 */
 
-var data = {
-  labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
-  series: [{ 
+$(document).ready(function() {
+  var data = {
+    labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
+    series: [{ 
              data: [2, 4, 6, 8, 10, 12]
           }]
-};
+  };
 
-var options = {}
+  var options = {}
 
-new Chartist.Line('.chart', data, options);
-
+  new Chartist.Line('.chart', data, options);
+});
 
 
 

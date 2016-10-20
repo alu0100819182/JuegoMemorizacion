5666f350b743d06871514b65f35ee0b59739c7f7
Omar Adolfo Alvarez Hernandez <alu0100819182@ull.edu.es>
2016-10-20 19:20:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -204,4 +204,13 @@ function downloadPDF() {
 }
 */
 
+$(document).ready(function() {
+	var PSV = PhotoSphereViewer({
+  		panorama:  'URL_DE_LA_IMAGEN',
+  		container: 'ID_DEL_DIV_DONDE_IRA_LA_IMAGEN',
+  		loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
+	});
+});
+
+
 

3be78628f80679cdfd6ce3fe74e19af9c96e4eb8
Omar Adolfo Alvarez Hernandez <alu0100819182@ull.edu.es>
2016-10-20 16:15:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -107,6 +107,7 @@ $(document).ready(function() {
 });
 */
 
+/*Waypoint
 $(document).ready(function() {
   var waypoint = new Waypoint({
     element: document.getElementById('tres'),
@@ -125,6 +126,18 @@ $(document).ready(function() {
     }
   });
 });
+*/
+
+var data = {
+  labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
+  series: [{ 
+             data: [2, 4, 6, 8, 10, 12]
+          }]
+};
+
+var options = {}
+
+new Chartist.Line('.chart', data, options);
 
 
 

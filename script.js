85e01a962dd25959760da446dc449a1c3fe97470
Omar Adolfo Alvarez Hernandez <alu0100819182@ull.edu.es>
2016-10-26 19:14:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-var tipo;
-var giro;
-var numeroGiro = 1;
-
 $(document).ready(function() {
   console.log("Empezamos");
   $('#card').flip({
     axis: 'y', // 'x' para giro en eje X
     trigger: 'click', // 'hover': giro sobre tarjeta
     speed: 500, // Velocidad del giro
-    var propio = 'manzanza';
-    if(numeroGiro == 2) && (tipo == propio) {
-      console.log("Bien");
-    }
-    else {
-      numeroGiro++;
-    }
   });
   $('#carddos').flip({
     axis: 'y', // 'x' para giro en eje X
     trigger: 'click', // 'hover': giro sobre tarjeta
     speed: 500, // Velocidad del giro
-     var propio = "manzanza";
-    if(numeroGiro == 2) && (tipo == propio) {
-      console.log("Bien");
-    }
-    else {
-      numeroGiro++;
-    }
   });
 });
+
+var tipo;
+var giro;
+var numeroGiro = 1;
+
+function igualar() {
+  console.log("hola");
+}

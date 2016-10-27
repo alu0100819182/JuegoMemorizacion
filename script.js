75a2f08413e9e5e3f97c267d575e915b0648269f
Omar Adolfo Alvarez Hernandez <alu0100819182@ull.edu.es>
2016-10-27 21:23:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
-  $(".example.stopwatch").TimeCircles({count_past_zero: false});
+  $(".example.stopwatch").TimeCircles({
+    count_past_zero: false,
+    "animation": "smooth",
+    "bg_width": 1.7,
+    "fg_width": 0.05,
+    "circle_bg_color": "#60686F",
+    "time": {
+        "Days": {
+            "text": "Days",
+            "color": "#FFCC66",
+            "show": false
+        },
+        "Hours": {
+            "text": "Hours",
+            "color": "#99CCFF",
+            "show": false
+        },
+        "Minutes": {
+            "text": "Minutes",
+            "color": "#BBFFBB",
+            "show": false
+        },
+        "Seconds": {
+            "text": "Seconds",
+            "color": "#FF9999",
+            "show": true
+        }
+    }
+  });
   $(".start").click(function(){ $(".example.stopwatch").TimeCircles().start(); });
   $(".stop").click(function(){ $(".example.stopwatch").TimeCircles().stop(); });
   $(".restart").click(function(){ $(".example.stopwatch").TimeCircles().restart(); });    

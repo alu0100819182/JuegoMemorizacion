c90c39d92b71f233c2ef77fb386b4b170fa655e8
Omar Adolfo Alvarez Hernandez <alu0100819182@ull.edu.es>
2016-10-27 18:31:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
-  $("#clock").sevenSeg({ value: 30 });
+    $("#DateCountdown").TimeCircles({
+    "animation": "smooth",
+    "bg_width": 1.2,
+    "fg_width": 0.1,
+    "circle_bg_color": "#60686F",
+    "time": {
+        "Days": {
+            "text": "Days",
+            "color": "#FFCC66",
+            "show": false
+        },
+        "Hours": {
+            "text": "Hours",
+            "color": "#99CCFF",
+            "show": false
+        },
+        "Minutes": {
+            "text": "Minutes",
+            "color": "#BBFFBB",
+            "show": true
+        },
+        "Seconds": {
+            "text": "Seconds",
+            "color": "#FF9999",
+            "show": true
+        }
+    }
+  });
   $('#card').flip({
     axis: 'y', // 'x' para giro en eje X
     trigger: 'click', // 'hover': giro sobre tarjeta
